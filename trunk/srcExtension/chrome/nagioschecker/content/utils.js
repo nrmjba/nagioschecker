9a86cb162c41a806b88ded7d4e935ab8aaa720ed
petr.simek <petr.simek@aac39b44-4a2c-0410-b179-3f88ddce5740>
2010-01-29 20:51:48 +0000

    new trunk

---
+++ trunk/srcExtension/chrome/nagioschecker/content/utils.js
@@ -0,0 +1,158 @@
+/*
+	parseUri 1.2.1
+	(c) 2007 Steven Levithan <stevenlevithan.com>
+	MIT License
+*/
+
+function parseUri (str) {
+	var	o   = parseUri.options,
+		m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+		uri = {},
+		i   = 14;
+
+	while (i--) uri[o.key[i]] = m[i] || "";
+
+	uri[o.q.name] = {};
+	uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+		if ($1) uri[o.q.name][$1] = $2;
+	});
+
+	return uri;
+};
+
+parseUri.options = {
+	strictMode: false,
+	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+	q:   {
+		name:   "queryKey",
+		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+	},
+	parser: {
+		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+	}
+};
+
+/*
+*
+* http://www.dustindiaz.com/getelementsbyclass/
+*
+*/
+function getElementsByClass(searchClass,node,tag) {
+	var classElements = new Array();
+	if ( node == null )
+		node = document;
+	if ( tag == null )
+		tag = '*';
+	var els = node.getElementsByTagName(tag);
+	var elsLen = els.length;
+	var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+	for (var i = 0, j = 0; i < elsLen; i++) {
+		if ( pattern.test(els[i].className) ) {
+			classElements[j] = els[i];
+			j++;
+		}
+	}
+	return classElements;
+}
+
+
+function createNagiosUrl(server,type,host,service) {
+	
+	var parsed = parseUri(server.urlstatus);
+	var url = parsed.protocol + '://' + parsed.authority + parsed.path;
+	var params = [];
+
+	if (type!='detail') {
+		if (parsed.queryKey['hostgroup']) {
+			params.push('hostgroup='+nagios_escape(parsed.queryKey['hostgroup']));
+			params.push('style=detail');
+		}
+		else if (parsed.queryKey['servicegroup']) {
+			params.push('servicegroup='+nagios_escape(parsed.queryKey['servicegroup']));
+			params.push('style=detail');			
+		}
+		else {
+			if (type=='host_problems') params.push('hostgroup=all');
+			else if (type=='service_problems') params.push('host=all');
+		}
+		
+	}
+	else url = url.replace(/status\.cgi/,'extinfo.cgi');
+	
+	switch (type) {
+		case 'host_problems':
+			params.push('style=hostdetail');
+			params.push('hoststatustypes=12');
+		break;
+		case 'service_problems':
+			params.push('servicestatustypes='+((server.versionOlderThan20) ? '248' : '28'));
+		break;
+		case 'detail':
+			params.push('type='+((service) ? '2' : '1'));
+			params.push('host='+nagios_escape(host));
+			if (service) params.push('service='+nagios_escape(service));
+		break;
+	}
+	
+	if (params.length>0) url += '?' + params.join('&');
+
+	return url;
+}
+
+function nagios_escape(url) {
+	return escape(url.replace(/ /,'+'));
+}
+
+function getUglyNodeValue(node,ids) {
+  var tmp = node;
+  for (var i=0;i<ids.length;i++) {
+    try {
+      tmp=tmp.childNodes[ids[i]];
+    }
+    catch (e) {
+      return false;
+    }
+  }
+  return tmp.nodeValue;
+}
+
+function getUglyNode(node,ids) {
+  var tmp = node;
+  for (var i=0;i<ids.length;i++) {
+    try {
+      tmp=tmp.childNodes[ids[i]];
+    }
+    catch (e) {
+      return false;
+    }
+  }
+  return tmp;
+}
+
+function alertMessage(header,mess) {
+	var bundle = document.getElementById("nch-strings");
+	var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].createInstance(Components.interfaces.nsIPromptService);
+	var result = prompts.alert(window, bundle.getString(header), bundle.getString(mess));
+}
+
+function confirmMessage(header,mess) {
+	var bundle = document.getElementById("nch-strings");
+	var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].createInstance(Components.interfaces.nsIPromptService);
+	return prompts.confirm(window, bundle.getString(header), bundle.getString(mess));
+}
+
+function getExtensionVersion() {
+  	var value = "";
+  	try {
+   		var RDFService = Components.classes["@mozilla.org/rdf/rdf-service;1"]
+             .getService(Components.interfaces.nsIRDFService);
+   		var extensionDS= Components.classes["@mozilla.org/extensions/manager;1"].getService(Components.interfaces.nsIExtensionManager).datasource;
+   		var target = extensionDS.GetTarget(RDFService.GetResource("urn:mozilla:item:{123b2220-59cb-11db-b0de-0800200c9a66}"), RDFService.GetResource("http://www.mozilla.org/2004/em-rdf#version"), true);
+   		value = target.QueryInterface(Components.interfaces.nsIRDFLiteral).Value;
+  	} catch(e) {
+  	}
+  	return value;
+ }
+
+
---
+++ trunk/srcExtension/defaults/preferences/prefs.js
@@ -0,0 +1,81 @@
+pref("extensions.{123b2220-59cb-11db-b0de-0800200c9a66}.description", "chrome://nagioschecker/locale/nagioschecker.properties");
+pref("extensions.{123b2220-59cb-11db-b0de-0800200c9a66}.name", "chrome://nagioschecker/locale/nagioschecker.properties");
+
+pref("extensions.nagioschecker.stopped", false);
+pref("extensions.nagioschecker.timeout", 30);
+
+pref("extensions.nagioschecker.info_type", 0);
+pref("extensions.nagioschecker.info_window_type", 1);
+pref("extensions.nagioschecker.show_statusbar_down", true);
+pref("extensions.nagioschecker.show_statusbar_unreachable", true);
+pref("extensions.nagioschecker.show_statusbar_unknown", true);
+pref("extensions.nagioschecker.show_statusbar_warning", true);
+pref("extensions.nagioschecker.show_statusbar_critical", true);
+
+pref("extensions.nagioschecker.show_window_column_information", true);
+pref("extensions.nagioschecker.show_window_column_alias", false);
+pref("extensions.nagioschecker.show_window_column_flags", false);
+pref("extensions.nagioschecker.show_window_column_attempt", false);
+pref("extensions.nagioschecker.show_window_column_status", true);
+
+pref("extensions.nagioschecker.statuses_translation", 0);
+
+
+pref("extensions.nagioschecker.sounds_by_type_down", true);
+pref("extensions.nagioschecker.sounds_by_type_unreachable", true);
+pref("extensions.nagioschecker.sounds_by_type_unknown", true);
+pref("extensions.nagioschecker.sounds_by_type_warning", true);
+pref("extensions.nagioschecker.sounds_by_type_critical", true);
+
+
+pref("extensions.nagioschecker.refresh", 5);
+pref("extensions.nagioschecker.refreshsec", 0);
+pref("extensions.nagioschecker.worktimefrom", "0:00");
+pref("extensions.nagioschecker.worktimeto", "23:59");
+pref("extensions.nagioschecker.workday_0", true); //sunday
+pref("extensions.nagioschecker.workday_1", true); 
+pref("extensions.nagioschecker.workday_2", true);
+pref("extensions.nagioschecker.workday_3", true);
+pref("extensions.nagioschecker.workday_4", true);
+pref("extensions.nagioschecker.workday_5", true);
+pref("extensions.nagioschecker.workday_6", true);
+
+pref("extensions.nagioschecker.one_window_only", true);
+pref("extensions.nagioschecker.blinking", 2);
+pref("extensions.nagioschecker.play_sound", 2);
+pref("extensions.nagioschecker.play_sound_attempt", 1);
+pref("extensions.nagioschecker.click", 0);
+
+pref("extensions.nagioschecker.filter_out_acknowledged", true);
+pref("extensions.nagioschecker.filter_out_disabled_notifications", false);
+pref("extensions.nagioschecker.filter_out_disabled_checks", true);
+pref("extensions.nagioschecker.filter_out_soft_states", false);
+pref("extensions.nagioschecker.filter_out_downtime", false);
+pref("extensions.nagioschecker.filter_out_services_on_down_hosts", false);
+pref("extensions.nagioschecker.filter_out_services_on_acknowledged_hosts", false);
+pref("extensions.nagioschecker.filter_out_all_down", false);
+pref("extensions.nagioschecker.filter_out_all_unreachable", false);
+pref("extensions.nagioschecker.filter_out_all_unknown", false);
+pref("extensions.nagioschecker.filter_out_all_warning", false);
+pref("extensions.nagioschecker.filter_out_all_critical", false);
+pref("extensions.nagioschecker.filter_out_regexp_hosts", false);
+pref("extensions.nagioschecker.filter_out_regexp_services", false);
+pref("extensions.nagioschecker.filter_out_regexp_info", false);
+pref("extensions.nagioschecker.filter_out_regexp_hosts_value", "");
+pref("extensions.nagioschecker.filter_out_regexp_hosts_reverse", false);
+pref("extensions.nagioschecker.filter_out_regexp_services_value", "");
+pref("extensions.nagioschecker.filter_out_regexp_services_reverse", false);
+pref("extensions.nagioschecker.filter_out_regexp_info_value", "");
+pref("extensions.nagioschecker.filter_out_regexp_info_reverse", false);
+pref("extensions.nagioschecker.filter_out_flapping", false);
+
+pref("extensions.nagioschecker.sound_warning", 0);
+pref("extensions.nagioschecker.sound_warning_path", "");
+pref("extensions.nagioschecker.sound_critical", 0);
+pref("extensions.nagioschecker.sound_critical_path", "");
+pref("extensions.nagioschecker.sound_down", 0);
+pref("extensions.nagioschecker.sound_down_path", "");
+pref("extensions.nagioschecker.version", "");
+
+pref("extensions.nagioschecker.prefer_text_config", false);
+pref("extensions.nagioschecker.prefer_text_config_type", 0);
